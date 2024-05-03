d8f782ee32a3d258a1dbac6aecb856a29be47b25
sinica <vadikanikanov@gmail.com>
2024-05-04 01:18:17 +0700

    work well

--- mega81Less/main.c
+++ mega81Less/main.c
@@ -15,11 +15,10 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <string.h>
-//#include "usart0.h"
 
 
-volatile char txBuff[4];
-volatile char rxBuff[4];
+volatile char txBuff[5];
+volatile char rxBuff[5];
 volatile int tx_index = 0;
 volatile int rx_index = 0;
 volatile int process_data = 0;
@@ -27,46 +26,42 @@ volatile int process_data = 0;
 char let = ' ';
 volatile uint8_t USART_ReceiveBuffer;
 
-ISR(USART_RX_vect){
-	cli();
-	
-	if (rx_index < 4){
+ISR(USART_RX_vect){  // прерыванию по окончанию приема модуля USART
+	if (rx_index < 5){
+	  while(!(UCSR0A&(1<<RXC0))){};
 	  rxBuff[rx_index] = UDR0;
 	  rx_index++;}
-	else{
+	if(rx_index >= 5){
 		//UCSR0B &= ~(1 << RXCIE0);
 		//UCSR0B |= (1 << TXCIE0);
-		UCSR0B |= (1 << UDRIE0) | (1 << TXCIE0);
+		UCSR0B |= (1 << UDRIE0) | (1 << TXCIE0);  // Разрешаем прерывание по опустошению - начинаем передачу массива
 		rx_index = 0;
 		process_data = 1;
 	}
 	
-	sei();
 }
 
-ISR (USART_TX_vect)
+ISR (USART_TX_vect)  // прерывание по завершению отправки
 {
-	PORTD &= ~0b00000100; // Отпускаем ножку направления PD2 по прерыванию завершения отправки данных
+	PORTD &= ~0b00000010; // Отпускаем ножку направления PD2 по прерыванию завершения отправки данных
 	UCSR0B &= ~(1 << TXCIE0);
 	UCSR0B |= (1 << RXCIE0);
 }
 
-ISR(USART_UDRE_vect)
+ISR(USART_UDRE_vect) //Прерывание по опустошению буффера УАПП
 {
-	cli();
-	PORTD |= 0b00000100; // Поднимаем ножку направления PD2 по прерыванию начала отправки данных
-	if(tx_index < 4){ 	// Вывели весь буффер?
+	PORTD |= 0b00000010; // Поднимаем ножку направления PD2 по прерыванию начала отправки данных
+	if(tx_index < 5){ 	// Вывели весь буффер?
 		while (!(UCSR0A & (1<<UDRE0)));
 		UDR0 = txBuff[tx_index];	// Берем данные из буффера.
 		tx_index++;
 	}
 	else{
 		tx_index = 0;
-		UCSR0B &= ~(1 << UDRIE0);
+		UCSR0B &= ~(1 << UDRIE0);  // Запрещаем прерывание по опустошению - передача закончена     
 		//UCSR0B &= ~(1 << TXCIE0);
 		//UCSR0B |= (1 << RXCIE0);
 	}
-	sei();
 }
 
 void init_port(void)
@@ -90,17 +85,7 @@ void init_usart(void)
 	sei();
 }
 
-// void tx_usart(uint8_t c)
-// {
-// 	//while(!(UCSR0A & ( 1 << UDRE0 ))); //waits until something was received
-// 	UDR0 = c;
-// }
-void tx_uart_string(char str[]){
-	//int len = strlen(str);
-	//for(int i=0;i<len;i++){
-	//	tx_usart(str[i]);
-	//}
-	//memcpy(txBuff, str, sizeof(str));
+void tx_uart_string(char* str){
 	for (int i = 0; i < 5; i ++){           
 		txBuff[i] = str[i];
 	}
@@ -110,35 +95,12 @@ void tx_uart_string(char str[]){
 }
 
 int waitGoal = 0;
-int main(void)
-{
-    init_port();
-	init_usart();
-	sei();
-	///tx_uart_string("pot");
-    while (1)
-    {
-		if(waitGoal = 0){
-			int currentSegment = rand()%3;
-			set_all_lamp(currentSegment);
-			waitGoal = 1;
-		}
-		wait_goal();
-		if (process_data == 1) {
-			tx_uart_string(rxBuff);
-			process_data = 0;
-		}
-    }
-    return 0;
-}
+
 
 void wait_goal(void){
-// 		 if(!(PINB & R0C1SensorPortB)){tx_uart_string("r0c1");waitGoal=0;_delay_ms(300);}
-// 	     if(!(PINB & R0C2SensorPortB)){tx_uart_string("r0c2");waitGoal=0;_delay_ms(300);}
-// 		 if(!(PINB & R0C0SensorPortB)){tx_uart_string("r0c0");waitGoal=0;_delay_ms(300);}
-		if(!(PINB & R0C1SensorPortB)){tx_uart_string("r0c1");waitGoal=0;_delay_ms(400);}	
-		if(!(PINB & R0C2SensorPortB)){tx_uart_string("r0c2");waitGoal=0;_delay_ms(400);}
-		if(!(PINB & R0C0SensorPortB)){tx_uart_string("r0c0");waitGoal=0;_delay_ms(400);}
+		if(!(PINB & R0C1SensorPortB)){tx_uart_string("r0c1");_delay_ms(800);waitGoal=0;}	
+		if(!(PINB & R0C2SensorPortB)){tx_uart_string("r0c2");_delay_ms(800);waitGoal=0;}
+		if(!(PINB & R0C0SensorPortB)){tx_uart_string("r0c0");_delay_ms(800);waitGoal=0;}
 	    
 }
 
@@ -149,4 +111,26 @@ void set_all_lamp(int currentSegment){
 	if(currentSegment==0) PORTB |= R0C0lampPortB;
 	if(currentSegment==1) PORTB |= R0C1lampPortB;
 	if(currentSegment==2) PORTC |= R0C2lampPortC;
+}
+int main(void)
+{
+    init_port();
+	init_usart();
+	sei();
+    while (1)
+    {
+		if(waitGoal = 0){
+			int currentSegment = rand()%3;
+			set_all_lamp(currentSegment);
+			waitGoal = 1;
+			_delay_ms(400);
+		}
+		wait_goal();
+		if (process_data == 1) {
+			_delay_ms(100);
+			tx_uart_string(&rxBuff);
+			process_data = 0;
+		}
+    }
+    return 0;
 }
